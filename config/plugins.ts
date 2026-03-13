cdd564bbd4539499d5d56661e8f9726c2d685fdb
MohamedPr0o4591 <ev9ry.24hours@gmail.com>
2026-03-13 05:55:11 +0200

    Disable cloud-cronjob-runner plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,11 @@
+// config/plugins.ts
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+    // ✅ عطّل الـ cloud-cronjob-runner
+    'cloud-cronjob-runner': {
+        enabled: false,
+    },
+});
 
 export default config;
---
+++ src/api/clirent/services/clirent.ts
@@ -0,0 +1,7 @@
+/**
+ * clirent service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::clirent.clirent');
